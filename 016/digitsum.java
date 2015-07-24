92af6579ac7fd56964aefe21c88da0f6ae8ec1c3
Son Nguyen <mail@gimu.org>
2015-07-24 22:10:12 +0200

    Add solution 016

---
+++ 016/digitsum.java
@@ -0,0 +1,25 @@
+import java.math.BigInteger;
+
+class DigitSum {
+
+    public static void solve(int power) {
+        BigInteger base = new BigInteger("2");
+        BigInteger result = base.pow(power); // Result of 2^power
+
+        BigInteger sum = BigInteger.ZERO;
+
+        while (!result.equals(BigInteger.ZERO)) {
+            BigInteger temp = result.divide(BigInteger.TEN);
+            BigInteger digit = result.subtract(temp.multiply(BigInteger.TEN));
+            result = temp;
+
+            sum = sum.add(digit);
+        }
+
+        System.out.println(sum);
+    }
+    
+    public static void main(String[] args) {
+        solve(1000);
+    }
+}
